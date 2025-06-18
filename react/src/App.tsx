7d8cb3d6b51b591fdf151cf5b5873df1348e1153
Kalo Jo <kalojo@Kalos-MacBook-Pro.local>
2025-06-18 11:49:35 -1000

    navbar has been added

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Testing to see if this works
       </p>
     </>
   )
