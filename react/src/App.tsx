382115819cd172dfd1e95c29160a86ad9613d315
Kalo Jo <kalojo@Kalos-MacBook-Pro.local>
2025-06-18 12:00:18 -1000

    barebones of front page is being constructed I have the general idea now I am implementing

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Testing to see if this works
-      </p>
+      <main>
+        <div className="center">
+            <h2>Welcome To My Website</h2>
+        </div>
+        <div>
+          <p>
+            What would you like to know about me?
+          </p>
+        </div>
+      </main>
+      <footer className="footer">
+        <a href="https://github.com/KdotJo">Github</a>
+        <a href="https://www.linkedin.com/in/kalo-jo-666935349">LinkedIn</a>
+        <a href="https://www.instagram.com/kalokjo">Instagram</a>
+        <a href="https://youtube.com/@KyunghoJo">Youtube</a>
+        <p>&copy; 2025 Kalo Jo</p>
+      </footer>
     </>
   )
 }
