4b2648747fa4c1af55c8d50cbf0b4c0d1b78337c
Kalo Jo <kalojo@Kalos-MacBook-Pro.local>
2025-06-18 14:49:45 -1000

    projects page basic

---
+++ react/src/pages/projects/projects.tsx
@@ -0,0 +1,19 @@
+function Projects() {
+    return (
+        <>
+            <h1>Projects</h1>
+            <div className="section1">
+                <p>
+                    Here are some of the projects I've worked on!
+                </p>
+            </div>
+            <div className="section2">
+                <p>
+                    Currently empty cause I'm sad!
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Projects
